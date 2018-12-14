7b582a0c58b506313d5a3bafd6bad5c13aa6a092
Michele D'Amico <31276401+damikael@users.noreply.github.com>
2018-12-14 16:16:34 +0100

    Update Setup.php

--- setup/Setup.php
+++ setup/Setup.php
@@ -390,7 +390,8 @@ class Setup {
         echo $colors->getColoredString("OK", "green"); 
 
         // apply simplesalphp patch for spid compliance
-        shell_exec("cp -rf " . $curDir . "/setup/simplesamlphp " . $curDir . "/vendor");
+        // override managed by composer
+        //shell_exec("cp -rf " . $curDir . "/setup/simplesamlphp/* " . $curDir . "/vendor/simplesamlphp");
 
         // write example files 
         if($addExamples) {
