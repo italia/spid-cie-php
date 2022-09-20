a16d37329df10e1a1abbb5677e06ee011b9e5ca2
damikael <michele.damico@linfaservice.it>
2022-09-20 10:31:17 +0200

    fix: metadata-signer.php

--- metadata-signer.php
+++ metadata-signer.php
@@ -5,14 +5,15 @@ require_once('vendor/simplesamlphp/simplesamlphp/config/authsources.php');
 
 use SimpleSAML\Metadata\Signer;
 
-$metadata_in = $argv[0] || 'metadata.xml';
-$metadata_out = $argv[1] || 'metadata-signed.xml';
-$metadata_service = $argv[2] || 'service';
+//php metadata-signer.php metadata.xml metadata-signed.xml service
+$metadata_in        = (count($argv) > 0 && $argv[1])? $argv[1] : 'metadata.xml';
+$metadata_out       = (count($argv) > 0 && $argv[2])? $argv[2] : 'metadata-signed.xml';
+$metadata_service   = (count($argv) > 0 && $argv[3])? $argv[3] : 'service';
 
-//$config['service']['privatekey'] = 'fake.pem';
-//$config['service']['certificate'] = 'fake.crt';
+$config[$metadata_service]['privatekey'] = 'spid-sp.pem';
+$config[$metadata_service]['certificate'] = 'spid-sp.crt';
 
 $signer = new Signer();
-$xml = file_get_contents('metadata.xml');
-$xml = Signer::sign($xml, $config['service'], 'SAML 2 SP');
-file_put_contents('metadata-signed.xml', $xml);
+$xml = file_get_contents($metadata_in);
+$xml = Signer::sign($xml, $config[$metadata_service], 'SAML 2 SP');
+file_put_contents($metadata_out, $xml);
